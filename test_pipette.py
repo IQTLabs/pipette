aa7d6f3d437ebf1b57bff40d5a1304af35076c0b
Josh Bailey <josh@vandervecken.com>
2020-02-05 10:45:05 +1300

    boilerplate.

--- test_pipette.py
+++ test_pipette.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python
 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import unittest
-from ryu.controller import dpset
-from ryu.controller.ofp_event import EventOFPMsgBase
 from pipette import Pipette
 
 
@@ -10,7 +21,7 @@ class PipetteSmokeTest(unittest.TestCase):  # pytype: disable=module-attr
     """Test bare instantiation of controller classes."""
 
     def test_smoke(self):
-        ryu_app = Pipette(dpset={})
+        _ = Pipette(dpset={})
 
 
 if __name__ == "__main__":
