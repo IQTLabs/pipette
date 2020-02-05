41165e5a5076eee10f92efd376f36c5087211bc8
Josh Bailey <josh@vandervecken.com>
2020-02-05 15:28:13 +1300

    Use /16.

--- pipetteconf.sh
+++ pipetteconf.sh
@@ -2,7 +2,7 @@
 COPROINT=enx0023565c8859
 # address fake services will be run on (will be proxied from real IPs)
 # must have the same prefix width as real network.
-NFVIP=192.168.101.1/24
+NFVIP=10.10.0.1/16
 # FAUCET VLAN where fake services will appear.
 VLAN=2
 # interface that will be created for fake services to run on.
