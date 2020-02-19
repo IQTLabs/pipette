b5ebc222bc37944913ef24159f9870e61d8c8a1d
ryan <rashley@iqt.org>
2020-02-19 06:58:11 -0500

    appease shellcheck

--- runpipette.sh
+++ runpipette.sh
@@ -88,7 +88,7 @@ fi
 remove_int_ip() {
   local int="$1"
   sudo ip addr flush dev "$int"
-  sudo ip link set $int up
+  sudo ip link set "$int" up
 }
 
 
@@ -96,36 +96,36 @@ remove_int_ip() {
 echo "Configuring OVS switch for pipette"
 sudo ip link add dev "$FAKEINT" type veth peer name "ovs$FAKEINT"
 for i in $COPROINT $FAKEINT ovs$FAKEINT ; do
-  remove_int_ip $i
+  remove_int_ip "$i"
 done
 
 for ((i=0; i< "${#VLANS[@]}"; i++)) ; do
   vlan="${VLANS[$i]}"
   nfvip="${NFVIPS[$i]}"
   fakeintvlan=${FAKEINT}.${vlan}
-  sudo ip link add link "$FAKEINT" name "$fakeintvlan" type vlan id $vlan
+  sudo ip link add link "$FAKEINT" name "$fakeintvlan" type vlan id "$vlan"
   sudo ip link set dev "$fakeintvlan" address "$FAKESERVERMAC"
   remove_int_ip "$fakeintvlan"
-  sudo ip addr add $nfvip dev "$fakeintvlan"
+  sudo ip addr add "$nfvip" dev "$fakeintvlan"
 done
 
 
-sudo ovs-vsctl --if-exists del-br $BR
+sudo ovs-vsctl --if-exists del-br "$BR"
 
 echo "Configuring bridge"
-sudo ovs-vsctl add-br $BR
+sudo ovs-vsctl add-br "$BR"
 echo "Adding ports"
-sudo ovs-vsctl add-port $BR $COPROINT -- set Interface $COPROINT ofport_request=$COPROPORT
-sudo ovs-vsctl add-port $BR ovs$FAKEINT -- set Interface ovs$FAKEINT ofport_request=$FAKEPORT
+sudo ovs-vsctl add-port "$BR" "$COPROINT" -- set Interface "$COPROINT" ofport_request=$"COPROPORT"
+sudo ovs-vsctl add-port "$BR" "ovs$FAKEINT" -- set Interface "ovs$FAKEINT" ofport_request="$FAKEPORT"
 echo "Setting controller"
-sudo ovs-vsctl set-controller $BR tcp:127.0.0.1:$OF
+sudo ovs-vsctl set-controller "$BR" tcp:127.0.0.1:"$OF"
 
 
 if [ $RECORD -ne 0 ]; then
     echo "Starting tcpdump on interface $COPROINT"
-    sudo tcpdump -i $COPROINT -F $PCAP_LOCATION -C $FILE_SIZE &
+    sudo tcpdump -i "$COPROINT" -F "$PCAP_LOCATION" -C "$FILE_SIZE" &
 fi
 
 
 # docker build -f $DFILE . -t cyberreboot/pipette && docker run -e NFVIPS=$NFVIPS -e FAKESERVERMAC=$FAKESERVERMAC -e FAKECLIENTMAC=$FAKECLIENTMAC -e VLANS=$VLANS -p 127.0.0.1:$OF:6653 -ti cyberreboot/pipette
-ryu-manager --verbose --ofp-tcp-listen-port $OF pipette.py
+ryu-manager --verbose --ofp-tcp-listen-port "$OF" pipette.py
